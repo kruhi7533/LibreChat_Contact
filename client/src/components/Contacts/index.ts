80b685440ecac265d0a587b96fe764fc59b6467b
kruhi7533 <kruhi7533@gmail.com>
2026-05-12 00:47:09 +0530

    feat: create index file for Contacts components export

---
+++ client/src/components/Contacts/index.ts
@@ -0,0 +1,8 @@
+export { default as ContactsPage } from './ContactsPage';
+export { default as ContactsList } from './ContactsList';
+export { default as ContactDetail } from './ContactDetail';
+export { default as ContactForm } from './ContactForm';
+export { default as ImportModal } from './ImportModal';
+export * from './types';
+export * from './api';
+export * from './queries';
